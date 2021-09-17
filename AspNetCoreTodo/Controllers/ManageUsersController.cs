ca53857968bf5df4d3b0d2abd6a6708337bd2d93
liuchunfeng <chunfeng.liu@innocellence.com>
2021-09-17 16:47:34 +0800

    feat: use role in Identity

---
+++ AspNetCoreTodo/Controllers/ManageUsersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Controllers
+{
+    [Authorize(Roles = Constants.AdministratorRole)]
+    public class ManageUsersController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ManageUsersController(UserManager<IdentityUser> userManager) 
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var admins = (await _userManager.GetUsersInRoleAsync(Constants.AdministratorRole)).ToArray();
+
+            var everyone = _userManager.Users.ToArray();
+
+            var model = new ManageUsersViewModel
+            {
+                Adminstrators = admins,
+                Everyone = everyone
+            };
+
+            return View(model);
+        }
+    } 
+}
--- AspNetCoreTodo/Program.cs
+++ AspNetCoreTodo/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AspNetCoreTodo
 {
@@ -13,7 +14,11 @@ namespace AspNetCoreTodo
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            InitializeDatabase(host);
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +27,24 @@ namespace AspNetCoreTodo
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        //此方法暂时不可用，需要弄清楚 IWebHost 如何被传入进来。下面的链接解决了这个问题
+        //https://stackoverflow.com/questions/59774559/how-do-i-get-a-instance-of-a-service-in-asp-net-core-3-1
+        public static void InitializeDatabase(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.InitializeAsync(services).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Error occured seeding the DB.");
+                }
+            }
+        }
     }
 }
