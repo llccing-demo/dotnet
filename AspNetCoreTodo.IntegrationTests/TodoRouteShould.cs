6c2edc0232122f23d4de1eb8eb1dbdbb454979bb
liuchunfeng <chunfeng.liu@innocellence.com>
2021-09-18 09:37:28 +0800

    feat: ad IntergrationTests

---
+++ AspNetCoreTodo.IntegrationTests/TodoRouteShould.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AspNetCoreTodo.IntegrationTests
+{
+    public class TodoRouteShould: IClassFixture<TestFixture>
+    {
+        private readonly HttpClient _client;
+
+        public TodoRouteShould(TestFixture fixture) 
+        {
+            _client = fixture.Client;
+        }
+
+        [Fact]
+        public async Task ChallengeAnonymousUser()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "/todo");
+
+            var response = await _client.SendAsync(request);
+
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+
+            Assert.Equal("http://localhost:8888/Identity/Account" + "/Login?ReturnUrl=%2Ftodo", response.Headers.Location.ToString());
+
+        }
+    }
+}
